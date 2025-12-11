0324cc98aaaf0d09cf49604f391873424f7c4fb5
Sofizam26 <sofizam2006@gmail.com>
2025-12-11 23:19:00 +0100

    Delete RedSocial directory

--- RedSocial/bd/bd.php
+++
@@ -1,6 +0,0 @@
-<?php
-$cadena_conexion = 'mysql:dbname=redsocial;host=127.0.0.1';
-$usuario = 'root';
-$clave = '';
-
-$bd = new PDO($cadena_conexion, $usuario, $clave);
--- RedSocial/cabecera.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <a href="home.php">Home</a>
-    <a href="buscar.php">Buscar</a>
-    <a href="perfil_propio.php">Mi Perfil</a>
-</header>
-    <link rel="stylesheet" href="CSS/cabecera.css">
-
-<hr>
